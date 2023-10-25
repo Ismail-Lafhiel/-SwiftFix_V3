a1e3b13d0a8cc1dfc3396dff3788143dc44c10f8
Ismail-Lafhiel <odashimidorida@gmail.com>
2023-10-25 16:37:24 +0100

    Created form validation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,170 +1,172 @@
-//burger menu
-const menu = document.querySelector('.menu');
-const burger = document.querySelector('.hamburger');
+////// ------------------------------------------------- start burger menu ------------------------------------------------------------//////////////
+
+const menu = document.querySelector(".menu");
+const burger = document.querySelector(".hamburger");
 
 burger.addEventListener("click", () => {
-    burger.classList.toggle("active")
-    menu.classList.toggle("active")
-    console.log("done");
-})
-//end burger menu
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+  console.log("done");
+});
+////// ------------------------------------------------- start burger menu ------------------------------------------------------------//////////////
 
-/* services dropdown */
+////// ------------------------------------------------- start services dropdown ------------------------------------------------------------//////////////
 
-const dropDown = document.querySelector(".fa-caret-down")
-const dropDownMenu = document.querySelector(".dropdown-menu")
+const dropDown = document.querySelector(".fa-caret-down");
+const dropDownMenu = document.querySelector(".dropdown-menu");
 
 dropDown.addEventListener("click", () => {
-    dropDownMenu.classList.toggle("active")
-})
+  dropDownMenu.classList.toggle("active");
+});
 
-/* end services dropdown */
+////// ------------------------------------------------- end services dropdown ------------------------------------------------------------//////////////
 
-// interactive search bar
+////// ------------------------------------------------- start interactive search bar ------------------------------------------------------------//////////////
 
 const search = document.querySelector(".search-field");
 const searchIcon = document.querySelector("#search-bar");
 searchIcon.addEventListener("click", () => {
-    search.classList.toggle("active");
-    console.log("operation done");
-})
-//end interactive search bar
-// start modal
-const modal = document.getElementById("modal")
-const openModal = () => {
-    modal.classList.add("open-modal");
-}
-const closeModal = () => {
-    modal.classList.remove("open-modal");   
-}
-// end modal
-// start FAQ
+  search.classList.toggle("active");
+  console.log("operation done");
+});
+////// ------------------------------------------------- end interactive search bar ------------------------------------------------------------//////////////
 
-const faPlus = document.querySelectorAll(".fa-plus")
-const answer = document.querySelectorAll(".answer")
+////// ------------------------------------------------- start modal ------------------------------------------------------------//////////////
+
+const modal = document.getElementById("modal");
+const openModal = () => {
+  modal.classList.add("open-modal");
+};
+const closeModal = () => {
+  modal.classList.remove("open-modal");
+};
+////// ------------------------------------------------- end modal ------------------------------------------------------------//////////////
+
+////// ------------------------------------------------- start FAQ ------------------------------------------------------------//////////////
+
+const faPlus = document.querySelectorAll(".fa-plus");
+const answer = document.querySelectorAll(".answer");
 faPlus.forEach((fa, index) => {
-    fa.addEventListener('click', () => {
-        answer[index].classList.toggle("active")
-    });
+  fa.addEventListener("click", () => {
+    answer[index].classList.toggle("active");
+  });
 });
 
-// end FAQ
+////// ------------------------------------------------- end FAQ ------------------------------------------------------------//////////////
+
+////------------------------------------------------- form validation ---------------------------------------------------//////////////
+
 // form handling
 const form = document.getElementById("form");
-form.addEventListener('submit', function (e) {
-    // prevent the form from submitting
-    e.preventDefault();
+const submit = document.getElementById("submit");
+const fullName = document.getElementById("full-name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
+const subject = document.getElementById("subject");
 
+const validateFullName = () => {
+  const nameError = document.getElementById("name-error");
+  if (fullName.value.trim() === "") {
+    handleError(fullName, nameError, "Full name is required");
+    return false;
+  } else if (!fullName.value.match(/^[A-Za-z]+\s{1}[A-Za-z]+$/)) {
+    handleError(fullName, nameError, "Invalid Name");
+    return false;
+  } else {
+    handleValid(fullName, nameError, "Valid");
+    return true;
+  }
+};
+const validateEmail = () => {
+  const emailError = document.getElementById("email-error");
+  if (email.value.trim() === "") {
+    handleError(email, emailError, "Email is required");
+    return false;
+  } else if (
+    !email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g)
+  ) {
+    handleError(email, emailError, "Invalid email");
+    return false;
+  } else {
+    handleValid(email, emailError);
+    return true;
+  }
+};
+
+const validatePhone = () => {
+  const phoneError = document.getElementById("phone-error");
+  if (phone.value.trim() === "") {
+    handleError(phone, phoneError, "Phone is required");
+    return false;
+  } else if (!phone.value.match(/^\+212[6-7]\d{8}$/)) {
+    handleError(phone, phoneError, "Invalid phone");
+    return false;
+  } else {
+    handleValid(phone, phoneError);
+    return true;
+  }
+};
+
+const validateMessage = () => {
+  const messageError = document.getElementById("msg-error");
+  if (message.value.trim() === "") {
+    handleError(message, messageError, "Message is required");
+    return false;
+  } else {
+    handleValid(message, messageError);
+    return true;
+  }
+};
+const validateSubject = () => {
+  const subjectError = document.getElementById("subject-error");
+  if (subject.value.trim() === "") {
+    handleError(subject, subjectError, "Subject is required");
+    return false;
+  } else {
+    handleValid(subject, subjectError);
+    return true;
+  }
+};
+
+fullName.addEventListener("input", validateFullName);
+email.addEventListener("input", validateEmail);
+phone.addEventListener("input", validatePhone);
+message.addEventListener("input", validateMessage);
+subject.addEventListener("input", validateSubject);
+
+const handleError = (input, err, errorMessage) => {
+  err.innerHTML = errorMessage;
+  err.classList.add("error");
+  input.classList.add("error");
+  err.classList.remove("valid");
+  input.classList.remove("valid");
+  input.classList.add("apply-shake");
+  setTimeout(() => {
+    input.classList.remove("apply-shake");
+  }, 1000);
+};
+
+const handleValid = (input, valid) => {
+  valid.innerHTML = "Valid";
+  valid.classList.add("valid");
+  input.classList.add("valid");
+  valid.classList.remove("error");
+  input.classList.remove("error");
+};
+form.addEventListener("submit",  (e)=> {
+  e.preventDefault();
+  if (
+    validateFullName() &&
+    validateEmail() &&
+    validatePhone() &&
+    validateMessage() &&
+    validateSubject()
+  ) {
+    submit.removeAttribute("disabled");
+    // alert("Form submitted successfully");
+  } else {
+    submit.setAttribute("disabled", true);
+    // alert("Form validation failed");
+  }
 });
-
-// error messages
-const nameError = document.getElementById("name-error")
-const phoneError = document.getElementById("phone-error")
-const emailError = document.getElementById("email-error")
-const subjError = document.getElementById("subj-error")
-const msgError = document.getElementById("msg-error")
-//icons
-const xmark = document.querySelector(".fa-xmark");
-const check = document.querySelector(".fa-check");
-const handleName = (xmark, check) => {
-    const fullName = document.getElementById("full-name").value
-    if (fullName == "") {
-        nameError.innerHTML = "Name is required"
-        nameError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false;
-    }
-    if (!fullName.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
-        nameError.innerHTML = "Invalid name"
-        nameError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false
-    }
-    nameError.innerHTML = "Valid"
-    nameError.style.color = 'green'
-    xmark.style.visibility = "hidden"
-    check.style.visibility = "visible"
-    return true;
-}
-
-const handleEmail = (xmark, check) => {
-    const email = document.getElementById("email").value
-    if (email == "") {
-        emailError.innerHTML = "Email is required"
-        emailError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false;
-    }
-    if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g)) {
-        emailError.innerHTML = "Invalid email"
-        emailError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false;
-    }
-    emailError.innerHTML = "Valid"
-    emailError.style.color = 'green'
-    xmark.style.visibility = "hidden"
-    check.style.visibility = "visible"
-    return true;
-}
-
-const handlePhone = (xmark, check) => {
-    const phone = document.getElementById("phone").value
-    if (phone.length == 0) {
-        phoneError.innerHTML = "Phone is required"
-        phoneError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false;
-    }
-    else if (phone.length !== 10) {
-        phoneError.innerHTML = "Phone should be 10 digits"
-        phoneError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false;
-    }
-    if (!phone.match(/^[0-9]{10}$/)) {
-        phoneError.innerHTML = "You should only enter digits"
-        phoneError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false;
-    }
-    else {
-        phoneError.innerHTML = "Valid"
-        phoneError.style.color = 'green'
-        xmark.style.visibility = "hidden"
-        check.style.visibility = "visible"
-        return true
-    }
-}
-
-const handleMsg = (xmark, check) => {
-    const msg = document.getElementById("message").value;
-    let require = 30
-    let left = require - msg.length
-    if (msg.length > 0 && msg.length < require) {
-        msgError.innerHTML = left + " more characters required"
-        msgError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false
-    }
-    else if(msg.length == 0){
-        msgError.innerHTML = "You have to enter a message"
-        msgError.style.color = 'red'
-        xmark.style.visibility = "visible"
-        check.style.visibility = "hidden"
-        return false
-    }
-    msgError.innerHTML = "Valid"
-    msgError.style.color = 'green'
-    xmark.style.visibility = "hidden"
-    check.style.visibility = "visible"
-    return true
-}
